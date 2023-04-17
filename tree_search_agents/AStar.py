cb03c91e7d28b8217ca9867d4536b0c19e608da7
imemmul <emrulurak17@outlook.com>
2023-04-18 01:29:26 +0300

    A* works but Grid5 gives different score

--- tree_search_agents/AStar.py
+++ tree_search_agents/AStar.py
@@ -24,28 +24,29 @@ class AStarAgent(TreeSearchAgent):
         # print(pq.queue)
         results = []
         for goal in env.get_goals():
+            env.reset()
             pq = PriorityQueue()  # priority queue to keep track of the frontier
-            pq.enqueue(([env.to_state(env.current_position)], [], 0), -self.get_heuristic(env, env.to_state(env.current_position), goal)) # insert the starting node with a priority of 0
+            pq.enqueue(([env.to_state(env.current_position)], [], 0), self.get_heuristic(env, env.to_state(env.current_position), goal)) # insert the starting node with a priority of 0
             visited = set()  # set to keep track of explored nodes
+            print(f"goal position {env.to_position(goal)}")
             while not pq.is_empty():
                 # print(f"whole queue {pq.queue}")
                 # print_queue(pq.queue, env)
                 # print(pq.queue)
                 curr_obj = pq.dequeue()
-                # print(f"where am i {env.current_position}")
-
                 curr_state = curr_obj[0][-1] # returns current state
                 env.set_current_state(curr_state)
-                
+                # print(f"where am i {env.current_position}")
                 # print(f"curr_state {curr_obj}")
                 curr_reward = curr_obj[2]
+                # print(f"pq {pq.queue}")
                 if curr_state == goal:
                     # print(f"get node type {env.get_node_type(env.to_position(curr_state))}")
-                    # for node in curr_obj[0]:
-                    #     print(f"positions {env.to_position(node)}")
-                    results.append((curr_obj[1], curr_obj[2], curr_obj[0]))
-                elif len(results) == len(env.get_goals()):
-                    return  max(results, key=lambda x: x[2])
+                    if (curr_obj[1], curr_obj[2], curr_obj[0]) not in results:
+                        results.append((curr_obj[1], curr_obj[2], curr_obj[0]))
+                        break
+                # elif len(results) == len(env.get_goals()):
+                #     return max(results, key=lambda x: x[2])
                 # print(f"curr node {curr_node}")
                 # print(f"i am in position of {env.to_position(curr_state)}")
                 # print(f"my action to achieve {curr_obj[1]}")
@@ -61,7 +62,7 @@ class AStarAgent(TreeSearchAgent):
                         new_path.append(next_state)
                         new_action_list = curr_obj[1].copy()
                         new_action_list.append(action)
-                        new_pri = -self.get_heuristic(env, next_state, goal) + new_reward # calculates straight_line distance and adds new_reward to it
+                        new_pri = self.get_heuristic(env, next_state, goal) + new_reward # calculates straight_line distance and adds new_reward to it
                         new_score = new_reward + curr_reward # keeps total score
                         pq.enqueue((new_path, new_action_list, new_score), new_pri)
                     # elif next_state in env.get_goals():
@@ -70,6 +71,8 @@ class AStarAgent(TreeSearchAgent):
                     # elif is_done and next_state not in env.get_goals():
                     #     pass
                     env.set_current_state(curr_state)
+        print(f"results {results}")
+        return max(results, key=lambda x: x[2])
 
     def get_heuristic(self, env: Environment, state: int, goal:int) -> float:
         """
@@ -86,7 +89,7 @@ class AStarAgent(TreeSearchAgent):
         # below is manhattan distance implementation
         goal_pos = env.to_position(goal)
         curr_pos = env.to_position(state)
-        return abs(goal_pos[0] - curr_pos[0]) + abs(goal_pos[1] - curr_pos[1])
+        return -(abs(goal_pos[0] - curr_pos[0]) + abs(goal_pos[1] - curr_pos[1]))
             
 
 
